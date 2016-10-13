96f2a84f59a189a38bdd0bd04d69945c916d369f
Vidar Einarsson <vidar10@ru.is>
2016-10-13 00:30:36 +0000

    Kata step 1 finished

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,1 +1,33 @@
-package 
+package tdd;
+
+public class Calculator
+{
+	public static int add(String numbers)
+	{
+		if(numbers.equals(""))
+			return 0;
+		else if(numbers.contains(","))
+			return sum(splitNumbers(numbers));
+		else
+			return 1;
+	}
+
+	private static int toInt(String number)
+	{
+		return Integer.parseInt(number);
+	}
+
+	private static String[] splitNumbers(String numbers)
+	{
+	    return numbers.split(",");
+	}
+      
+    private static int sum(String[] numbers)
+    {
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+	}
+}
---
+++ src/test/CalculatorTest.java
@@ -0,0 +1,31 @@
+package tdd;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest
+{
+    @Test
+    public void testEmptyString()
+    {
+        assertEquals(0, Calculator.add(""));
+          
+    }
+
+    @Test
+	public void testOneNumber() 
+	{
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() 
+	{
+		assertEquals(3, Calculator.add("1,2"));
+	}
+	@Test
+    public void testMultipleNumbers()
+    {
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+}
